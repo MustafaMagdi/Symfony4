9f3608b29973759ec5b0ab99946cc0351e003e0c
Mustafa Magdi <developer.mustafa@gmail.com>
2018-03-04 19:20:23 +0400

    update

--- src/Controller/IndexController.php
+++ src/Controller/IndexController.php
@@ -15,6 +15,6 @@ class IndexController extends AbstractController
      */
     public function index()
     {
-        return $this->render('article/base.html.twig');
+        return $this->render('index/index.html.twig');
     }
 }
